3ceb4c336a88fb12894c0d568310b67ef0035815
Ben Ogren <ben@benogren.com>
2025-01-15 19:36:36 -0700

    update mongo

--- src/app/actions/saveEmail.ts
+++ src/app/actions/saveEmail.ts
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 import { EmailModel } from "@/app/models/emails";
 
 export async function saveEmail(email: JSON) {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(process.env.MONGODB_URI as string);
     await EmailModel.create(email);
 }
