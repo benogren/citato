a2843f145812510fc099b4a4d3e4bf2bd1534b81
Ben Ogren <ben@benogren.com>
2025-01-15 18:19:31 -0700

    update to title again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="container mx-auto my-12 text-center">
-      <h1 className="title text-2xl">Welcome to Citato</h1>
+      <h1 className="title text-2xl">Citato</h1>
     </div>
   );
 }
